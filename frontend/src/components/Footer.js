1d3c70c5d0c5b60003c5a028a712ed52b21646c6
Darshil0109 <122811740+Darshil0109@users.noreply.github.com>
2024-08-11 15:16:32 +0530

    Add Responsive Landing page

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,14 +3,14 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="container text-center py-4">
+    <footer className="footer text-2xl md:text-3xl lg:text-2xl">
+      <div className="container text-center py-4 flex flex-col items-center">
         <img src="./media/logo.png" alt="Logo" className="footer-logo" />
         <p className="mt-3">© 2024 SwiftBids. All rights reserved.</p>
         <div className="social-links mt-3">
-          <a href="/" className="social-link me-3">Facebook</a>
-          <a href="/" className="social-link me-3">Twitter</a>
-          <a href="/" className="social-link">Instagram</a>
+          <a href="/" className="social-link me-3 md:text-2xl">Facebook <i class="fas fa-brands fa-facebook-messenger"></i></a>
+          <a href="/" className="social-link me-3 md:text-2xl">Twitter <i class="fas fa-brands fa-twitter"></i></a>
+          <a href="/" className="social-link md:text-2xl" >Instagram <i class="fas fa-brands fa-instagram"></i></a>
         </div>
         <div className="contact-info mt-3">
           <p>Email: support@swiftbids.com</p>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,52 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState ,useEffect} from 'react'
 
 const Navbar = () => {
   const [isMenuOpen,setMenu]=useState(false)
-  console.log(isMenuOpen)
+  useEffect(() => {
+    document.body.style.overflow = isMenuOpen ? 'hidden' : 'auto';
+  }, [isMenuOpen]);
   return (
-    <>
-      <div className="h-32 md:h-20  w-screen bg-dark-subtle-400 sticky flex justify-between px-8 py-3 items-center  z-50">
-      <div className="md:h-16 md:w-16 h-20 w-20" >
-        <img src="./media/logo.png" alt="logo"/>
-      </div>
-    
-      <ul className="md:flex items-center justify-content-evenly gap-5 hidden">
-        <li><a className="hover:text-gray-700 font-semibold" href="/products">Auctions</a></li>
-        <li className="hover:text-gray-700 font-semibold"><a href="/">About</a></li>
-        <li className="hover:text-gray-700 font-semibold"><a href="/">Help</a></li>
-        <li className="hover:text-gray-700 font-semibold"><a href="/">Contact</a></li>
-      </ul>
+      <div style={{
+        fontFamily: "Roboto",
+    }}>
+        <div className="h-34 sm:h-30 md:h-28 lg:h-26  w-screen bg-dark-subtle-400 sticky flex justify-between px-8 py-3 items-center  z-50">
+        <div className="h-20 w-20 sm:h-26 sm:w-26 md:h-24 md:w-24 lg:h-22 lg:w-22" >
+          <img src="./media/logo.png" alt="logo"/>
+        </div>
       
-      <div className="flex gap-8 items-center">
-        <button className="items-center bg-orange-500 text-white h-10 w-24 rounded hover:bg-orange-600  md:flex hidden gap-0.5 justify-between p-2 " type="button"><span className="font-medium text-white">sign up</span> <i className="fas fa-thin fa-right-to-bracket text-white "
+        <ul className="md:flex items-center justify-content-evenly gap-5 hidden">
+          <li ><a className="hover:text-gray-700 font-bold md:text-3xl  lg:text-2xl" href="/products">Auctions</a></li>
+          <li ><a className="hover:text-gray-700 font-bold md:text-3xl lg:text-2xl" href="/about">About</a></li>
+          <li ><a className="hover:text-gray-700 font-bold md:text-3xl lg:text-2xl" href="/help">Help</a></li>
+          <li ><a className="hover:text-gray-700 font-bold md:text-3xl lg:text-2xl" href="/contact">Contact</a></li>
+        </ul>
+        
+        <div className="flex gap-8 items-center">
+          <button className="md:h-22 md:w-46 lg:h-14 lg:w-32 md:flex hidden items-center bg-orange-500 text-white  rounded hover:bg-orange-600  gap-0.5 md:gap-4 lg:gap-1 justify-evenly p-2 " type="button"><span className="md:text-2xl lg:text-xl font-medium text-white text-center">sign up</span> <i className="fas fa-thin fa-right-to-bracket text-white md:text-2xl lg:text-xl lg:block hidden"
+            aria-hidden="true"></i>
+          </button>
+          {/* bar button */}
+          <button className="md:hidden items-center md:h-10 md:w-10 h-15 w-15" onClick={()=>{setMenu(!isMenuOpen)}} >
+            {isMenuOpen ? <i class="fas fa-solid fa-x text-4xl"></i> : <i className="fas fa-solid fa-bars text-4xl"></i>}  
+          </button>
+        </div>
+
+      </div>
+      <div style={{backgroundColor:'#FFFCF2'}} className={`md:hidden w-full h-full  absolute flex flex-col  justify-around ${isMenuOpen ? 'translate-y-0' : '-translate-y-full'} transition-transform duration-300 ease-in-out z-10`}>
+        <ul className="flex flex-col items-center justify-content-evenly gap-5 top-30">
+          <li><a className="hover:text-gray-700 font-semibold text-4xl" href="/">Auctions</a></li>
+          <li className="hover:text-gray-700 font-semibold text-4xl"><a href="/">About</a></li>
+          <li className="hover:text-gray-700 font-semibold text-4xl"><a  href="/">Help</a></li>
+          <li className="hover:text-gray-700 font-semibold text-4xl"><a  href="/">Contact</a></li>
+        </ul>
+        <div className="flex justify-center">
+          <button className="items-center bg-orange-500 text-white h-20 w-5/12 rounded hover:bg-orange-600 flex gap-0.5 p-2 justify-evenly" type="button"><span className="font-medium text-3xl text-white">sign up</span> <i className="fas fa-thin fa-right-to-bracket text-white text-3xl"
           aria-hidden="true"></i>
-        </button>
-        <button className="md:hidden items-center md:h-10 md:w-10 h-15 w-15" onClick={()=>{setMenu(!isMenuOpen)}} >
-          {isMenuOpen ? <i class="fas fa-solid fa-x text-4xl"></i> : <i className="fas fa-solid fa-bars text-4xl"></i>}
-          
-          
-        </button>
-      </div>
+          </button>
 
-    </div>
-     <div className={`md:hidden w-full h-full  absolute flex flex-col  justify-around ${isMenuOpen ? 'translate-y-0' : '-translate-y-full'} transition-transform duration-300 ease-in-out z-10`}>
-      <ul className="flex flex-col items-center justify-content-evenly gap-5 top-30">
-        <li><a className="hover:text-gray-700 font-semibold text-4xl" href="/">Auctions</a></li>
-        <li className="hover:text-gray-700 font-semibold text-4xl"><a href="/">About</a></li>
-        <li className="hover:text-gray-700 font-semibold text-4xl"><a  href="/">Help</a></li>
-        <li className="hover:text-gray-700 font-semibold text-4xl"><a  href="/">Contact</a></li>
-      </ul>
-      <div className="flex justify-center">
-        <button className="items-center bg-orange-500 text-white h-20 w-5/12 rounded hover:bg-orange-600 flex gap-0.5 p-2 justify-evenly" type="button"><span className="font-medium text-3xl text-white">sign up</span> <i className="fas fa-thin fa-right-to-bracket text-white text-3xl"
-        aria-hidden="true"></i>
-        </button>
-
-      </div>
+        </div>
+        
       
+      </div>
     
     </div>
-    
-    </>
   )
 }
 
